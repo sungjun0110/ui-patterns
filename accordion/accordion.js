04be31992158816d6692d0c7a0adc21263d9712c
Sungjun Choi <sungjun0110@gmail.com>
2022-03-02 14:20:38 -0800

    adds arrowHandler for accordion

--- accordion/accordion.js
+++ accordion/accordion.js
@@ -2,10 +2,25 @@ const tabElms = document.getElementsByClassName('tab');
 
 for (let i = 0; i < tabElms.length; i++) {
     tabElms[i].addEventListener('click', (tab) => {
-        openHandler(tab.target.nextElementSibling);
+        openHandler(tab);
     })
 }
 
-function openHandler(pane) {
-    pane.classList.toggle('closed');
+function openHandler(tab) {
+    closeTabs();
+    tab.target.nextElementSibling.classList.toggle('closed');
+    arrowHandler(tab);
+}
+
+function closeTabs() {
+    for (let i = 0; i < tabElms.length; i++) {
+        tabElms[i].nextElementSibling.classList.add('closed');
+    }
+}
+
+function arrowHandler(tab) {
+    for (let i = 0; i < tabElms.length; i++) {
+        tabElms[i].childNodes[1].classList.add('closed');
+    }
+    tab.target.childNodes[1].classList.remove('closed');
 }
