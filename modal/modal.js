fa96d2873a9110372cfa38c403784473c254a8f4
Sungjun Choi <sungjun0110@gmail.com>
2022-03-02 22:36:05 -0800

    adds modal functionality

--- modal/modal.js
+++ modal/modal.js
@@ -1,7 +1,23 @@
 const tileElms = document.getElementsByClassName('tile');
+const buttonElms = document.getElementsByTagName('button');
 
 for (let i = 0; i < tileElms.length; i++) {
-    tileElms[i].addEventListener('click', () => {
-        
+    tileElms[i].addEventListener('click', (tile) => {
+        clickHandler(tile);       
     })
+}
+
+for (let i = 0; i < buttonElms.length; i++) {
+    buttonElms[i].addEventListener('click', (button) => {
+        closePage(button.target.parentElement.parentElement);
+    })
+}
+
+function clickHandler(tile) {
+    console.log(tile);
+    tile.target.children[1].classList.add('active');
+}
+
+function closePage(pageElem) {
+    pageElem.classList.remove('active');
 }
