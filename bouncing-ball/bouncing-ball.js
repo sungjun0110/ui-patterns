60a21f3e53d5348f6fed84c5e6a815eef7ed85d4
Sungjun Choi <sungjun0110@gmail.com>
2022-03-07 22:15:34 -0800

    fixes variable errors

--- bouncing-ball/bouncing-ball.js
+++ bouncing-ball/bouncing-ball.js
@@ -4,16 +4,17 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 
 let x, y, vx, vy, offsetX, offsetY, elasticity, radius, color;
-let gravity = 980;
+let gravity = 9.8 * 600;
 let isOut = false, isDragging = false;
+
+vy = 0;
+vx = 0;
+offsetX = 0;
+offsetY = 0;
 
 function drawBall(newX, newY, newRadius, newColor, newElasticity) {
     x = newX;
     y = newY;
-    vy = 0;
-    vx = 0;
-    offsetX = 0;
-    offsetY = 0;
     elasticity = newElasticity;
     radius = newRadius;
     color = newColor;
@@ -26,7 +27,6 @@ function drawBall(newX, newY, newRadius, newColor, newElasticity) {
 }
 
 function fall() {
-    console.log('x=', x, ' y=', y, ' vx=', vx, ' vy=', vy, ' radius=', radius);
     if (y + radius >= canvas.height) {
         if (Math.abs(vy) <= 0.1) {
             vy = 0;
@@ -43,6 +43,7 @@ function fall() {
     drawBall(x, y, radius, color, elasticity);
 };
 
+// calculate the ball speed in Y axis
 function calculateVy(elasticity = 1.0, direction = '') {
     if (direction === 'hit') {
         vy = -vy * elasticity;
@@ -51,6 +52,7 @@ function calculateVy(elasticity = 1.0, direction = '') {
     }
 }
 
+// calculate new position Y of a ball
 function calculateY() {
     y += (vy/60) + gravity/7200;
     if (y + radius >= canvas.height) {
@@ -64,5 +66,5 @@ function animate() {
     fall();
 }
 
-drawBall(canvas.width/2, 0, 70, 'blue', 0.555);
+drawBall(canvas.width/2, 0, 70, 'blue', .7);
 animate();
