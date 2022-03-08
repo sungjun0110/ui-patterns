3d37b912014fa30d873dfeb1197c3a5082821362
Sungjun Choi <sungjun0110@gmail.com>
2022-03-07 20:42:49 -0800

    adds a function to draw a ball

--- bouncing-ball/bouncing-ball.js
+++ bouncing-ball/bouncing-ball.js
@@ -3,9 +3,15 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 
-function initCanvas() {
-    ctx.fillStyle = 'rgb(200, 0, 0)';
-    ctx.fillRect(10, 10, 50, 50);
+let x, y, vx, vy, offsetX, offsetY, elasticity, radius, color;
+let isOut = false, isDragging = false;
+
+function drawBall(x, y, radius, color, elasticity) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.closePath();
 }
 
-initCanvas();
+drawBall(canvas.width/2, 0, 70, 'blue', 0.555);
