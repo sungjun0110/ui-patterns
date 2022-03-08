193f6a512d3243e470f6b6213366e9f1480e2d13
Sungjun Choi <sungjun0110@gmail.com>
2022-03-07 22:01:03 -0800

    adds falling animation

--- bouncing-ball/bouncing-ball.js
+++ bouncing-ball/bouncing-ball.js
@@ -4,9 +4,20 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 
 let x, y, vx, vy, offsetX, offsetY, elasticity, radius, color;
+let gravity = 980;
 let isOut = false, isDragging = false;
 
-function drawBall(x, y, radius, color, elasticity) {
+function drawBall(newX, newY, newRadius, newColor, newElasticity) {
+    x = newX;
+    y = newY;
+    vy = 0;
+    vx = 0;
+    offsetX = 0;
+    offsetY = 0;
+    elasticity = newElasticity;
+    radius = newRadius;
+    color = newColor;
+
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, Math.PI * 2);
     ctx.fillStyle = color;
@@ -14,4 +25,44 @@ function drawBall(x, y, radius, color, elasticity) {
     ctx.closePath();
 }
 
+function fall() {
+    console.log('x=', x, ' y=', y, ' vx=', vx, ' vy=', vy, ' radius=', radius);
+    if (y + radius >= canvas.height) {
+        if (Math.abs(vy) <= 0.1) {
+            vy = 0;
+            y = canvas.height - radius;
+        }
+        if (Math.abs(vy) > 0.1) {
+            calculateVy(elasticity, 'hit');
+            calculateY();
+        } 
+    } else {
+        calculateVy(elasticity);
+        calculateY();
+    }
+    drawBall(x, y, radius, color, elasticity);
+};
+
+function calculateVy(elasticity = 1.0, direction = '') {
+    if (direction === 'hit') {
+        vy = -vy * elasticity;
+    } else {
+        vy += gravity/60;
+    }
+}
+
+function calculateY() {
+    y += (vy/60) + gravity/7200;
+    if (y + radius >= canvas.height) {
+        y = canvas.height - radius;
+    }
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    fall();
+}
+
 drawBall(canvas.width/2, 0, 70, 'blue', 0.555);
+animate();
