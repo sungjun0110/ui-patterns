a021eee84d19aa07597f68ab42f4076e34f1eef6
Sungjun Choi <sungjun0110@gmail.com>
2022-03-08 22:59:45 -0800

    animation on drop down menu

--- drop-down-menu-btn/drop-down-menu-btn.js
+++ drop-down-menu-btn/drop-down-menu-btn.js
@@ -1,6 +1,7 @@
 const container = document.getElementById('container');
 const menuBtn = document.getElementById('menu-btn');
 const btnBackground = document.getElementsByClassName('btn-background')[0];
+const dropdownMenu = document.getElementById('dropdown-menu');
 const menuDiv = document.getElementsByClassName('dropdown-content')[0];
 const menuArr = document.getElementsByTagName('a');
 
@@ -10,18 +11,32 @@ container.addEventListener('mouseleave', () => {
 
 menuBtn.addEventListener('mouseenter', () => {
     menuActiveHandler();
-    btnBackgroundHandler();
+    btnBackgroundHandler('active');
 })
 
 function menuActiveHandler() {
     menuDiv.classList.toggle('show');
+    setTimeout(() => {
+        menuDiv.classList.toggle('open');
+    }, 250);
 }
 
-function btnBackgroundHandler() {
+function btnBackgroundHandler(status) {
     btnBackground.classList.toggle('active');
+    if (status === 'active') {
+        setTimeout(() => {
+            dropdownMenu.style.height = '9.5em';
+        }, 50);
+    } else {
+        dropdownMenu.style.height = '0em';
+    }
 }
 
 function deactiveHandler() {
     menuActiveHandler();
     btnBackgroundHandler();
 }
+
+// for (let i = 0; i < menuArr.length; i++) {
+//     menuArr[i].addEventListener('')
+// }
