6a758f39943454b938e876450f271aa5005107e2
Sungjun Choi <sungjun0110@gmail.com>
2022-03-08 21:53:26 -0800

    adds button handlers

--- drop-down-menu-btn/drop-down-menu-btn.js
+++ drop-down-menu-btn/drop-down-menu-btn.js
@@ -1,11 +1,27 @@
+const container = document.getElementById('container');
 const menuBtn = document.getElementById('menu-btn');
+const btnBackground = document.getElementsByClassName('btn-background')[0];
 const menuDiv = document.getElementsByClassName('dropdown-content')[0];
 const menuArr = document.getElementsByTagName('a');
 
-menuBtn.addEventListener('click', () => {
+container.addEventListener('mouseleave', () => {
+    deactiveHandler();
+})
+
+menuBtn.addEventListener('mouseenter', () => {
     menuActiveHandler();
+    btnBackgroundHandler();
 })
 
 function menuActiveHandler() {
     menuDiv.classList.toggle('show');
+}
+
+function btnBackgroundHandler() {
+    btnBackground.classList.toggle('active');
+}
+
+function deactiveHandler() {
+    menuActiveHandler();
+    btnBackgroundHandler();
 }
